b4889a26c03bb394f0850e0b50c4dfa99c0e787c
Chris Williams <chris@iterativedesigns.com>
2013-12-22 18:34:36 -0500

    Adding index and status.

--- server.js
+++ server.js
@@ -8,13 +8,7 @@ var app = express();
 
 app.get('/toggle', function(req, res){
   request({url:  SPARK+"/v1/devices/"+DEVICE_ID+"/toggle", method: "POST", form: {access_token: ACCESS_TOKEN}}, function (e, r, body) {
-    body = (JSON.parse(body));
-    console.log(body);
-    if (body.return_value == 1) {
-      res.send("Lights ON");
-    } else {
-      res.send("Lights OFF");
-    }
+    res.redirect("/");
   });
 });
 
