5f2f256b9578352d7244387646409964bada51f7
Chris Williams <chris@iterativedesigns.com>
2013-12-22 17:33:59 -0500

    Update.

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
 var DEVICE_ID = process.env.DEVICE_ID;
 var ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+var request = require("request");
+var SPARK = 'https://api.spark.io';
+
+var express = require('express');
+var app = express();
+
+app.get('/toggle', function(req, res){
+  request.post({url: SPARK+"/v1/devices/"+DEVICE_ID+"/toggle", body: {}}, function (e, r, body) {
+    console.log(body);
+    res.send("OK");
+  });
+});
+
+app.listen(process.env.PORT || 3000);
