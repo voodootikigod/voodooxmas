9603eb44a98a055428d8e07b34eb32b54e4b9a5c
Chris Williams <chris@iterativedesigns.com>
2013-12-24 09:15:43 -0500

    Adding icon and allowing it to live as an app when added to home screen.

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var SPARK = 'https://api.spark.io';
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/toggle', function(req, res){
   request({url:  SPARK+"/v1/devices/"+DEVICE_ID+"/toggle", method: "POST", form: {access_token: ACCESS_TOKEN}}, function (e, r, body) {
     res.redirect("/");
@@ -18,7 +20,7 @@ app.get("/", function (req, res) {
   request({url:  SPARK+"/v1/devices/"+DEVICE_ID+"/status", method: "POST", form: {access_token: ACCESS_TOKEN}}, function (e, r, body) {
     body = (JSON.parse(body));
     // display the status (line page writer).
-    res.send("<html><head><title>Voodoo Lights</title><link rel='stylesheet' href='//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css'></head><body style='text-align: center'><h1>Lights "+(body.return_value == 1 ? "ON" : "OFF")+"</h1><br/><br/><br/><br/><a href='/toggle'>*</a></body></html>");
+    res.send("<html><head><title>Voodoo Lights</title><meta name='apple-mobile-web-app-capable' content='yes' /><link rel='stylesheet' href='//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css'></head><body style='text-align: center'><h1>Lights "+(body.return_value == 1 ? "ON" : "OFF")+"</h1><br/><br/><br/><br/><a class='btn btn-default' href='/toggle'>Switch</a></body></html>");
   });
 })
 
